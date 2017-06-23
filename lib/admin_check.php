421e9959a1d659f7f9a85f82018ec112902561fa
Adam Quinton <adam.quinton@familysearch.org>
2017-06-23 10:29:34 -0600

    secure
    https://tools.ietf.org/html/rfc1918

--- lib/admin_check.php
+++ lib/admin_check.php
@@ -1,9 +1,9 @@
 <?php
 function same_subnet() {
-  return (substr($_SERVER['REMOTE_ADDR'], 0, 4) === '192.' ||
+  return (substr($_SERVER['REMOTE_ADDR'], 0, 8) === '192.168.' ||
       substr($_SERVER['REMOTE_ADDR'], 0, 3) === '10.' ||
-      substr($_SERVER['REMOTE_ADDR'], 0, 4) === '172.' ||
-      substr($_SERVER['REMOTE_ADDR'], 0, 4) === '127.' ||
+      substr($_SERVER['REMOTE_ADDR'], 0, 7) === '172.16.' ||
+      substr($_SERVER['REMOTE_ADDR'], 0, 9) === '127.0.0.1' ||
       $_SERVER['REMOTE_ADDR'] == '::1');
 }
 ?>
